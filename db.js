1189b2fc0b1a7ed267135f43e2048a54ead2ea34
Cristhian Vargas Flores <cristhiansergiovargasflores@gmail.com>
2026-04-23 16:31:53 -0400

    db.js actulizados para PRODUCCION

--- db.js
+++ db.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 // Creamos un "Pool" de conexiones. 
 // Es una forma eficiente de manejar múltiples peticiones a la base de datos al mismo tiempo.
 const pool = new Pool({
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
 });
 
 module.exports = pool;
